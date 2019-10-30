6d2c37c807c43229deee95b8da33d0d6f4376205
Luis Pérez <luis.perez@geekdacity.com>
2019-10-29 22:38:04 -0600

    Fix problem with import json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', loadChildren: './cars/cars.module#CarsModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'cars' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
