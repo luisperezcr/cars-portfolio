9a589793463eb8f863706af907d32d5e68e3d607
Luis Pérez <luis.perez@geekdacity.com>
2019-10-29 23:12:11 -0600

    Generate car item

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { CarsListComponent } from './containers/cars-list/cars-list.component';
 import { CarsService } from './services/cars.service';
 import { CarsRoutingModule } from './cars-routing.module';
+import { CarsItemComponent } from './components/cars-item/cars-item.component';
 
 
 
 @NgModule({
-  declarations: [CarsListComponent],
+  declarations: [CarsListComponent, CarsItemComponent],
   imports: [
     CommonModule,
     CarsRoutingModule
---
+++ src/app/cars/components/cars-item/cars-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cars-item',
+  templateUrl: './cars-item.component.html',
+  styleUrls: ['./cars-item.component.scss']
+})
+export class CarsItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
