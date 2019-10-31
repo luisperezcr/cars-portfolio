7296fcf9d3a0281428cde1fd827f2f7caf09400e
Luis Pérez <luis.perez@geekdacity.com>
2019-10-30 23:05:02 -0600

    Compare tool progress

--- src/app/cars/components/cars-compare-tool/cars-compare-tool.component.ts
+++ src/app/cars/components/cars-compare-tool/cars-compare-tool.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { CompareState } from '../../../state/compare.state';
 import { Car } from '../../models/car.interface';
+import { RemoveAll } from '../../../actions/compare.actions';
 
 @Component({
   selector: 'app-cars-compare-tool',
@@ -13,11 +14,21 @@ export class CarsCompareToolComponent implements OnInit {
   @Select(CompareState.getCars)
   cars$: Observable<Car>;
 
-  constructor() { }
+  car_1: Car;
+  car_2: Car;
+  car_3: Car;
+
+  constructor(private store: Store) { }
 
   ngOnInit() {
-    this.cars$.subscribe((car: any) => {
-      console.log(car);
+    this.cars$.subscribe((cars: any) => {
+      this.car_1 = cars[0];
+      this.car_2 = cars[1];
+      this.car_3 = cars[2];
     });
+  }
+
+  onGoBack() {
+    this.store.dispatch(new RemoveAll());
   }
 }
